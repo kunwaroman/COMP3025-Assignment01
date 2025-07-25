f7542d6eb0f24a003b4ef458848b8c96fb32503c
kunwaroman <omankunwar052@gmail.com>
2025-07-24 20:02:41 -0400

    Implemented MainActivity with ViewModel and adapter

--- app/src/main/java/ca/georgiancollege/assignment01_kunwar/view/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/assignment01_kunwar/view/MainActivity.kt
@@ -1,18 +1,38 @@
 package ca.georgiancollege.assignment01_kunwar.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.georgiancollege.assignment01_kunwar.adapter.MovieAdapter
 import ca.georgiancollege.assignment01_kunwar.databinding.ActivityMainBinding
+import ca.georgiancollege.assignment01_kunwar.viewmodel.MovieViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MovieViewModel by viewModels()
+    private val apiKey = "1a7582ac"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Setup RecyclerView layout manager
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Search button click triggers movie search
+        binding.searchButton.setOnClickListener {
+            val query = binding.searchEditText.text.toString().trim()
+            if (query.isNotEmpty()) {
+                viewModel.searchMovies(query, apiKey)
+            }
+        }
+
+        // Observe movies LiveData and update RecyclerView adapter
+        viewModel.movies.observe(this) { movieList ->
+            binding.recyclerView.adapter = MovieAdapter(movieList)
         }
     }
+}
